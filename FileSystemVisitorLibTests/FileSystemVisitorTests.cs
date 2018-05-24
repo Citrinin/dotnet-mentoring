a6b2b4325f6bfe8b56ff94748ad00cee2549369a
MINSK\Katsiaryna_Kuzkina <kate.kuzkina@gmail.com>
2018-05-24 17:25:33 +0300

    some tests added

--- FileSystemVisitorLibTests/FileSystemVisitorTests.cs
+++ FileSystemVisitorLibTests/FileSystemVisitorTests.cs
@@ -15,15 +15,15 @@ namespace FileSystemVisitorLib.Tests
         [TestInitialize]
         public void Setup() => _mockFileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
         {
-            { @"c:\myfile.txt", new MockFileData("My favorite file") },
-            { @"c:\test\jQuery.js", new MockFileData("some js") },
+            { @"c:\test\myfolder\coolstory.txt", new MockFileData("Cool story") },
             { @"c:\test\1.txt", new MockFileData("123") },
             { @"c:\test\image.gif", new MockFileData(new byte[] { 0x01, 0x02, 0x03, 0x04 }) },
-            { @"c:\test\myfolder\coolstory.txt", new MockFileData("Cool story") }
+            { @"c:\test\jQuery.js", new MockFileData("some js") },
+            { @"c:\myfile.txt", new MockFileData("My favorite file") },
         });
 
         [TestMethod()]
-        public void GetDirectoryContentTestCorrectData1()
+        public void GetDirectoryContentTest_CorrectPath1()
         {
             var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
             var result = fileSystemVisitor.GetDirectoryContent(@"c:\");
@@ -31,7 +31,7 @@ namespace FileSystemVisitorLib.Tests
         }
 
         [TestMethod()]
-        public void GetDirectoryContentTestCorrectData2()
+        public void GetDirectoryContentTest_CorrectPath2()
         {
             var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
             var result = fileSystemVisitor.GetDirectoryContent(@"c:\test\");
@@ -39,7 +39,7 @@ namespace FileSystemVisitorLib.Tests
         }
 
         [TestMethod()]
-        public void GetDirectoryContentTestCorrectDataWithFilter1()
+        public void GetDirectoryContentTest_CorrectPath_CorrectFilter1()
         {
             var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.EndsWith(".txt"));
             var result = fileSystemVisitor.GetDirectoryContent(@"c:\test\");
@@ -47,7 +47,7 @@ namespace FileSystemVisitorLib.Tests
         }
 
         [TestMethod()]
-        public void GetDirectoryContentTestCorrectDataWithFilter2()
+        public void GetDirectoryContentTest_CorrectPath_CorrectFilter2()
         {
             var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("test"));
             var result = fileSystemVisitor.GetDirectoryContent(@"c:\");
@@ -55,7 +55,7 @@ namespace FileSystemVisitorLib.Tests
         }
 
         [TestMethod()]
-        public void GetDirectoryContentTestCorrectDataWithFilterIsNll()
+        public void GetDirectoryContentTest_CorrectPath_FilterIsNll()
         {
             var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, null);
             var result = fileSystemVisitor.GetDirectoryContent(@"c:\");
@@ -64,10 +64,18 @@ namespace FileSystemVisitorLib.Tests
 
         [TestMethod()]
         [ExpectedException(typeof(ArgumentException))]
-        public void GetDirectoryContentTestIncorrectInputData2()
+        public void GetDirectoryContentTest_IncorrectPath()
         {
             var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
-            var result = fileSystemVisitor.GetDirectoryContent(@"c").Count();
+            var result = fileSystemVisitor.GetDirectoryContent("c").Count();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDirectoryContentTest_PathNull()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
+            var result = fileSystemVisitor.GetDirectoryContent(null).Count();
         }
 
         [TestMethod()]
@@ -93,6 +101,194 @@ namespace FileSystemVisitorLib.Tests
 
             Assert.AreEqual(7, result);
             Assert.AreEqual(true, eventHandled);
+        }
+
+        [TestMethod()]
+        public void FileFindedEventTest_ExcludeFromResult()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
+            fileSystemVisitor.FileFinded += (sender, args) =>
+            {
+                if (args.FullName.EndsWith(".txt"))
+                {
+                    args.ExcludeItemFromResult = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").Count();
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        public void FileFindedEventTest_StopSearch()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
+            fileSystemVisitor.FileFinded += (sender, args) =>
+            {
+                if (args.FullName.EndsWith("1.txt"))
+                {
+                    args.CancelSearching = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(4, result.Count);
+            Assert.AreEqual(@"c:\test\1.txt", result.Last());
+        }
+
+        [TestMethod()]
+        public void DirectoryFindedEventTest_ExcludeFromResult()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
+            fileSystemVisitor.DirectoryFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("test"))
+                {
+                    args.ExcludeItemFromResult = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(5, result.Count);
+        }
+
+        [TestMethod()]
+        public void DirectoryFindedEventTest_StopSearch()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem);
+            fileSystemVisitor.DirectoryFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("folder"))
+                {
+                    args.CancelSearching = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(@"c:\test\myfolder", result.Last());
+        }
+
+        [TestMethod()]
+        public void FileFindedEventTest_ExcludeFromResultWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("test"));
+            fileSystemVisitor.FileFinded += (sender, args) =>
+            {
+                if (args.FullName.EndsWith(".txt"))
+                {
+                    args.ExcludeItemFromResult = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").Count();
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        public void FileFindedEventTest_StopSearchWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("test"));
+            fileSystemVisitor.FileFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("story"))
+                {
+                    args.CancelSearching = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(@"c:\test\myfolder\coolstory.txt", result.Last());
+        }
+
+        [TestMethod()]
+        public void DirectoryFindedEventTest_ExcludeFromResultWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("test"));
+            fileSystemVisitor.DirectoryFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("test"))
+                {
+                    args.ExcludeItemFromResult = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod()]
+        public void DirectoryFindedEventTest_StopSearchWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => !s.Contains("test"));
+            fileSystemVisitor.DirectoryFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("test"))
+                {
+                    args.CancelSearching = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(@"c:\test", result.Last());
+        }
+        //----
+
+        [TestMethod()]
+        public void FilteredFileFindedEventTest_ExcludeFromResultWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("test"));
+            fileSystemVisitor.FilteredFileFinded += (sender, args) =>
+            {
+                if (args.FullName.EndsWith(".gif") || args.FullName.EndsWith(".js"))
+                {
+                    args.ExcludeItemFromResult = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").Count();
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        public void FilteredFileFindedEventTest_StopSearchWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("myfolder"));
+            fileSystemVisitor.FilteredFileFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("story"))
+                {
+                    args.CancelSearching = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(@"c:\test\myfolder\coolstory.txt", result.Last());
+        }
+
+
+        [TestMethod()]
+        public void FilteredDirectoryFindedEventTest_ExcludeFromResultWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Length >= 15);
+            fileSystemVisitor.FilteredDirectoryFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("folder"))
+                {
+                    args.ExcludeItemFromResult = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod()]
+        public void FilteredDirectoryFindedEventTest_StopSearchWithFilter()
+        {
+            var fileSystemVisitor = new FileSystemVisitor(_mockFileSystem, s => s.Contains("test"));
+            fileSystemVisitor.FilteredDirectoryFinded += (sender, args) =>
+            {
+                if (args.FullName.Contains("folder"))
+                {
+                    args.CancelSearching = true;
+                }
+            };
+            var result = fileSystemVisitor.GetDirectoryContent(@"c:\").ToList();
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(@"c:\test\myfolder", result.Last());
         }
 
     }
