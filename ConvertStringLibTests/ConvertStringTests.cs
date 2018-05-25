41cb47e7a1c97b3f8d4eb46e7ecb7892a0c9ebe2
MINSK\Katsiaryna_Kuzkina <kate.kuzkina@gmail.com>
2018-05-25 16:05:53 +0300

    tests added

--- ConvertStringLibTests/ConvertStringTests.cs
+++ ConvertStringLibTests/ConvertStringTests.cs
@@ -1,30 +1,74 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ConvertStringLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConvertStringLib.Tests
 {
     [TestClass()]
     public class ConvertStringTests
     {
+
         [TestMethod()]
-        public void ConvertToIntTest()
+        public void ConvertToIntTest_CorrectInput()
         {
             var converter = new ConvertString();
-            var result = converter.ConvertToInt("-");
-            Assert.AreEqual(0, result);
+            var result = converter.ConvertToInt("500");
+            Assert.AreEqual(500, result);
+        }
+
+        [TestMethod()]
+        public void ConvertToIntTest_CorrectInputBelowZero()
+        {
+            var converter = new ConvertString();
+            var result = converter.ConvertToInt("-500");
+            Assert.AreEqual(-500, result);
+        }
+
+        [TestMethod()]
+        public void ConvertToIntTest_CorrectInputWithSign()
+        {
+            var converter = new ConvertString();
+            var result = converter.ConvertToInt("+500");
+            Assert.AreEqual(500, result);
+        }
+
+        [TestMethod()]
+        public void ConvertToIntTest_CorrectInputIntMax()
+        {
+            var converter = new ConvertString();
+            var result = converter.ConvertToInt(int.MaxValue.ToString());
+            Assert.AreEqual(int.MaxValue, result);
+        }
+
+        [TestMethod()]
+        public void ConvertToIntTest_CorrectInputIntMin()
+        {
+            var converter = new ConvertString();
+            var result = converter.ConvertToInt(int.MinValue.ToString());
+            Assert.AreEqual(int.MinValue, result);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConvertToIntTest_ContainsLetter()
+        {
+            var converter = new ConvertString();
+            converter.ConvertToInt("+0d");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void ConvertToIntTestInputNull()
+        public void ConvertToIntTest_InputNull()
         {
             var converter = new ConvertString();
-            var result = converter.ConvertToInt(null);
+            converter.ConvertToInt(null);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConvertToIntTest_InputTooLong()
+        {
+            var converter = new ConvertString();
+            converter.ConvertToInt("12345678912");
         }
     }
 }
